1be21a76b71e5ca127dc43c6530ba40a54b53686
HadhemiBM <benmansourhadhemi99@gmail.com>
2024-11-04 10:32:27 +0100

    carousel

--- src/app/components/HeaderSection/index.tsx
+++ src/app/components/HeaderSection/index.tsx
@@ -3,6 +3,9 @@ import "react-responsive-carousel/lib/styles/carousel.min.css"; // Carousel styl
 import styles from './index.module.css';
 import Image from 'next/image';
 import dme from '../../Assests/dme.gif'
+import redu from '../../Assests/svg/redu.svg'
+import edge from '../../Assests/edge.webp'
+import { BarChart, Card } from '@tremor/react'
 const Header = () => {
   const settings = {
     showArrows: true,
@@ -12,10 +15,11 @@ const Header = () => {
     showStatus: false,
     interval: 3000
   };
-
+ 
   return (
     <div className={styles.carr}>
       <Carousel {...settings}>
+        {/* carousel 1  */}
       <div className={styles.container}>
       <div className={styles.HeaderText}>
     {/* <h1  className={styles.HeaderTitle}>4InA Technologie</h1> */}
@@ -27,26 +31,56 @@ const Header = () => {
       <Image className={styles.HeadImage} src = {dme} alt="IA" />
     </div>
     </div>
+        {/* carousel 2  */}
+
     <div className={styles.container}>
       <div className={styles.HeaderText}>
     {/* <h1  className={styles.HeaderTitle}>4InA Technologie</h1> */}
 
-      <h1  className={styles.HeaderTitle}>Redefining Energy Management with AI and IoT Excellence</h1>
-      <p className={styles.HeaderDesc}>Elevating energy management by integrating AI and IoT, enabling proactive monitoring, real-time analytics, and smarter decision-making to enhance your operational efficiency</p>
+      <h1  className={styles.HeaderTitle}> IMPACT OF THE SYSTEM
+ENERGY CONTROL ON
+THE INDUSTRY </h1>
+      <h3  className={styles.HeaderTitle3}> 20% Consumption Reduction </h3>
+      <h3  className={styles.HeaderTitle31}> 80% Reduction in Lost Time Duttes </h3>
+
+      <h3  className={styles.HeaderTitle32}> 50% Cost Reduction </h3>
+      <h3  className={styles.HeaderTitle33}> 80% Increased Productivity Time </h3>
+      {/* <p className={styles.HeaderDesc}>Elevating energy management by integrating AI and IoT, enabling proactive monitoring, real-time analytics, and smarter decision-making to enhance your operational efficiency</p> */}
       </div>
-    <div className={styles.Imagecontainer}>
-      <Image className={styles.HeadImage} src = {dme} alt="IA" />
+    <div className={styles.Imagecontaine2}>
+    <Image className={styles.HeadImage2} src = {redu} alt="IA" />
+ 
     </div>
     </div>
-    <div className={styles.container}>
+        {/* carousel 3  */}
+
+        <div className={styles.container3}>
       <div className={styles.HeaderText}>
     {/* <h1  className={styles.HeaderTitle}>4InA Technologie</h1> */}
 
-      <h1  className={styles.HeaderTitle}>Redefining Energy Management with AI and IoT Excellence</h1>
-      <p className={styles.HeaderDesc}>Elevating energy management by integrating AI and IoT, enabling proactive monitoring, real-time analytics, and smarter decision-making to enhance your operational efficiency</p>
+      <h1  className={styles.HeaderTitle}> Advantages of Installing Our Solution </h1>
+      <div className={styles.rowloa}>
+      <div className={styles.loader}></div>
+      <h3  className={styles.HeaderTitle4}> Monitoring of energy consumption </h3>
       </div>
-    <div className={styles.Imagecontainer}>
-      <Image className={styles.HeadImage} src = {dme} alt="IA" />
+      <div className={styles.rowloa}>
+      <div className={styles.loader}></div>
+      <h3  className={styles.HeaderTitle4}> Guidance for energy managers</h3>
+      </div>
+      <div className={styles.rowloa}>
+      <div className={styles.loader}></div>
+      <h3  className={styles.HeaderTitle4}> Monitoring Machine Behavior and Predicting Failures</h3>
+      </div>
+      <div className={styles.rowloa}>
+      <div className={styles.loader}></div>
+      <h3  className={styles.HeaderTitle4}> Reducing Maintenance Costs</h3>
+      </div>
+
+      {/* <p className={styles.HeaderDesc}>Elevating energy management by integrating AI and IoT, enabling proactive monitoring, real-time analytics, and smarter decision-making to enhance your operational efficiency</p> */}
+      </div>
+    <div className={styles.Imagecontainer3}>
+    <Image className={styles.HeadImage3} src = {edge} alt="IA" />
+ 
     </div>
     </div>
       </Carousel>
@@ -56,90 +90,3 @@ const Header = () => {
 
 export default Header;
 
-// "use client";
-
-// import Link from "next/link";
-// import React from "react";
-// import Slider from "react-slick";
-// import Image from "next/image";
-// import styles from "./index.module.css";
-// import dme from "../../Assests/dme.gif";
-
-// const Header: React.FC = () => {
-//   // Slick carousel settings
-//   const settings = {
-//     dots: true,
-//     infinite: true,
-//     speed: 500,
-//     slidesToShow: 1,
-//     slidesToScroll: 1,
-//     autoplay: true,
-//     autoplaySpeed: 3000,
-//   };
-
-//   return (
-   
-//       <Slider {...settings}>
-//         {/* Slide 1 */}
-//         <div className={styles.slide}>
-//         <div className={styles.HeaderText}>
-//           <h1 className={styles.HeaderTitle}>
-//             1Redefining Energy Management with AI and IoT Excellence
-//           </h1>
-//           <p className={styles.HeaderDesc}>
-//             Elevating energy management by integrating AI and IoT, enabling
-//             proactive monitoring, real-time analytics, and smarter
-//             decision-making to enhance your operational efficiency
-//           </p>
-//         </div>
-//         <div className={styles.Imagecontainer}>
-//           <Image className={styles.HeadImage} src={dme} alt="AI and IoT" />
-//         </div>
-//       </div>
-
-//         {/* Slide 2 */}
-//         <div className={styles.slide}>
-//           <div className={styles.HeaderText}>
-//             <h1 className={styles.HeaderTitle}>
-//               Optimizing Resources with Advanced AI Solutions
-//             </h1>
-//             <p className={styles.HeaderDesc}>
-//               Our technology maximizes energy use, reduces costs, and supports
-//               sustainable growth through tailored AI-powered insights.
-//             </p>
-//           </div>
-//           <div className={styles.Imagecontainer}>
-//             <Image
-//               className={styles.HeadImage}
-//               src={dme}
-//               alt="AI Optimization"
-//             />
-//           </div>
-//         </div>
-
-//         {/* Slide 3 */}
-//         <div className={styles.slide}>
-//           <div className={styles.HeaderText}>
-//             <h1 className={styles.HeaderTitle}>
-//               Unlocking the Potential of IoT in Modern Infrastructure
-//             </h1>
-//             <p className={styles.HeaderDesc}>
-//               Enhance your operations by leveraging IoT for seamless
-//               connectivity, real-time data collection, and improved asset
-//               management.
-//             </p>
-//           </div>
-//           <div className={styles.Imagecontainer}>
-//             <Image
-//               className={styles.HeadImage}
-//               src={dme}
-//               alt="IoT Infrastructure"
-//             />
-//           </div>
-//         </div>
-//       </Slider>
-
-//   );
-// };
-
-// export default Header;
