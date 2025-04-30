aa3e6063a80d711680976b35148412b625ee2f0f
HadhemiBm <benmansourhadhemi99@gmail.com>
2025-04-30 10:34:03 +0200

    err

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+
+import Head from "next/head";
+
+import AOS from 'aos';
+import Splash from "./home/page";
+
+export default function Home() {
+  return (
+    <div className="page">
+   <Head>
+        <meta name="google-site-verification" content="5qRlOQNaJ771i6hFI2xzZQ15dkBy2MUUDkqrzTzYq6A" />
+      </Head>
+      <Splash />
+    </div>
+  );
+}
