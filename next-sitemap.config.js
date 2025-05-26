0bd64f24b108b468ba9db7525427c2c2136b38a3
HadhemiBm <benmansourhadhemi99@gmail.com>
2025-05-26 11:01:12 +0200

    Robot

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://4inatechnology.com',
+  generateRobotsTxt: true, // (will generate robots.txt automatically)
+  changefreq: 'monthly',
+  priority: 0.7,
+  sitemapSize: 5000,
+};
